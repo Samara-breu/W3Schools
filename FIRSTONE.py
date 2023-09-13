61b073c67c74a23e1ef63632570c2c50b8e23871
Samara-breu <143050545+Samara-breu@users.noreply.github.com>
2023-09-12 21:10:23 -0300

    Add files via upload

---
+++ FIRSTONE.py
@@ -0,0 +1,59 @@
+"""
+códigos w3schools
+tudo o que é possível aprender
+de python (essa é uma forma de escrever um
+comentário maior que uma só linha)
+"""
+
+x = str(3) #x vai ser '3'
+y = int(3) #y vai ser 3
+z = float(3) #z vai ser 3.0
+
+#os comandos abaixo descrevem qual o tipo da variável
+print(type(x))
+print(type(y))
+
+"""
+Variáveis ​​de string podem ser declaradas usando aspas simples ou duplas:
+x = "John"
+é o mesmo que:
+x = 'John'
+"""
+
+A = 'many'
+a = 3
+
+#os nomes das variáveis ​​diferenciam maiúsculas de minúsculas
+
+"""
+Regras para variáveis ​​​​Python:
+- O nome de uma variável deve começar com uma letra ou sublinhado
+- Um nome de variável não pode começar com um número
+- Um nome de variável só pode conter caracteres alfanuméricos e sublinhados (Az, 0-9 e _)
+- Os nomes das variáveis ​​diferenciam maiúsculas de minúsculas (idade, Idade e IDADE são três variáveis ​​diferentes)
+- Um nome de variável não pode ser nenhuma das palavras-chave do Python
+"""
+
+#estojo de camelo
+myBeautifulVar = 6
+#caso pascal
+MyBeautifulVar = 8
+#caso de cobra
+my_beautiful_var = 10
+
+#Python permite atribuir valores a múltiplas variáveis ​​em uma linha:
+J, Q, K = 'oi', 'como ce ta?', 'espero que bem'
+print(J)
+print(Q)
+print(K)
+#você pode atribuir o mesmo valor a múltiplas variáveis ​​em uma linha:
+ais, dois, tres = 220
+print(ais)
+print(dois)
+print(tres)
+
+fruits = ["apple", "banana", "cherry"]
+i, e, u = fruits
+print(i)
+print(e)
+print(u)
